7a9cef8387584d17bb7f82c1c14d8725d4e863da
vkriznar <krizovid@gmail.com>
2021-06-24 10:32:00 +0200

    Add folder table to db

--- app/crud/models.py
+++ app/crud/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey, UniqueConstraint
 
 
 Base = declarative_base()
@@ -12,3 +13,12 @@ class User(Base):
     username = Column(String(30), unique=True, index=True)
     hashed_password = Column(String(64), nullable=False)
     salt = Column(String(32), nullable=False)
+
+
+class Folder(Base):
+    __tablename__ = "folder"
+    __table_args__ = (UniqueConstraint("user_id", "name", name="unique_user_id_name"),)
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    name = Column(String(30), nullable=False)
