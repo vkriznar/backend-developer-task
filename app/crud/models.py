aad2f66ec78fe17438b2c72cb44b6853e5d71ec5
vkriznar <krizovid@gmail.com>
2021-06-24 10:27:39 +0200

    Add name column to user table

--- app/crud/models.py
+++ app/crud/models.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(30), nullable=False)
     username = Column(String(30), unique=True, index=True)
     hashed_password = Column(String(64), nullable=False)
     salt = Column(String(32), nullable=False)
